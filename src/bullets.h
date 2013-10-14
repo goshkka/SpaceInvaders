34d1fa01f9473c0f7a7e6b0a4ed49c94aa9cef26
Derek Carr <zaphinath@gmail.com>
2013-10-13 18:31:54 -0600

    Adding blank blank bullets for cleanup

--- src/bullets.h
+++ src/bullets.h
@@ -13,4 +13,7 @@
 int tankBulletSymbol[TANK_BULLET_HEIGHT];
 int alienBulletSymbol[NUMBER_ALIEN_BULLETS][ALIEN_BULLET_HEIGHT];
 
+int blankTankBulletSymbol[TANK_BULLET_HEIGHT];
+int blankAlienBulletSymbol[ALIEN_BULLET_HEIGHT];
+
 #endif /* BULLETS_H_ */
